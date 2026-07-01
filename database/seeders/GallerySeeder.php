f5590b66f1509ddfc628a9a5d2cb3709af80485d
firdahanaa <firdahanaa@users.noreply.github.com>
2026-07-01 10:35:53 +0700

    Fix GallerySeeder hardcoded user_id and posyandu_id to prevent foreign key constraint violations

--- database/seeders/GallerySeeder.php
+++ database/seeders/GallerySeeder.php
@@ -9,10 +9,16 @@ class GallerySeeder extends Seeder
 {
     public function run()
     {
+        $user = \App\Models\User::first();
+        $userId = $user ? $user->id : 1;
+
+        $posyandu = \App\Models\Posyandu::first();
+        $posyanduId = $posyandu ? $posyandu->id : 1;
+
         DB::table('galleries')->insert([
             [
-                'posyandu_id' => 1,
-                'user_id' => 2,
+                'posyandu_id' => $posyanduId,
+                'user_id' => $userId,
                 'title' => 'Kegiatan Posyandu Januari',
                 'description' => 'Foto-foto kegiatan posyandu bulan Januari',
                 'photo' => 'kegiatan-januari.jpg',
@@ -21,8 +27,8 @@ class GallerySeeder extends Seeder
                 'updated_at' => now(),
             ],
             [
-                'posyandu_id' => 1,
-                'user_id' => 2,
+                'posyandu_id' => $posyanduId,
+                'user_id' => $userId,
                 'title' => 'Imunisasi Campak',
                 'description' => 'Foto kegiatan imunisasi campak',
                 'photo' => 'imunisasi-campak.jpg',
@@ -31,8 +37,8 @@ class GallerySeeder extends Seeder
                 'updated_at' => now(),
             ],
             [
-                'posyandu_id' => 2,
-                'user_id' => 2,
+                'posyandu_id' => $posyanduId,
+                'user_id' => $userId,
                 'title' => 'Penyuluhan Gizi',
                 'description' => 'Foto kegiatan penyuluhan gizi',
                 'photo' => 'penyuluhan-gizi.jpg',
