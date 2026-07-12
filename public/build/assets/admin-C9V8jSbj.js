8a1896752e58dad97ee69dceb8ed2a24ee47ef25
zikri <zikrizildan77@gmail.com>
2026-07-12 22:58:34 +0700

    fix: menghapus folder vendor dari repositori agar github actions menginstall dependensi dengan benar (perbaikan permissions dan path)

--- public/build/assets/admin-C9V8jSbj.js
+++
@@ -1,1 +0,0 @@
-import"./app-UyRVujZY.js";import{C as r,r as t}from"./chart-DU5eDjOK.js";r.register(...t);window.Chart=r;
--- public/build/assets/charts-CPA9ZYB3.js
+++
@@ -1,1 +0,0 @@
-import{C as r,r as a}from"./chart-DU5eDjOK.js";r.register(...a);window.Chart=r;
